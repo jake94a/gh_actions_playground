bb45f0a85524b0cc14563fb49bc3fd385156bbd5
Jake Anderson <jake94a@gmail.com>
2022-09-29 14:43:45 -0600

    paths ignore

--- .github/workflows/compare.py
+++ .github/workflows/compare.py
@@ -1,8 +1,22 @@
 import sys
 
 f = open("ignore-files.txt")
-ignore_files = f.readlines()
-for line in ignore_files:
-    print("line", line)
+ignore_files = f.read().splitlines()
+f.close()
+all_changed_files = sys.argv[1:]
 
-print("args: ", sys.argv)
+# file_diff = [
+#     x
+#     for x in ignore_files + all_changed_files
+#     if x not in ignore_files or x not in all_changed_files
+# ]
+
+# print("file_diff", file_diff)
+
+compared = []
+for changed_file_name in all_changed_files:
+    if changed_file_name not in ignore_files:
+        compared.append(changed_file_name)
+        break
+
+print("compared", compared)
